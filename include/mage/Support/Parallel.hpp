80aa4e9bfdf098cdac478c4f7f7459ec7f259fc6
Leandro A. Lacerda Campos <leandrolcampos@yahoo.com.br>
2026-06-09 22:42:40 -0300

    Add parallel work-item execution support
    Signed-off-by: Leandro A. Lacerda Campos <leandrolcampos@yahoo.com.br>

---
+++ include/mage/Support/Parallel.hpp
@@ -0,0 +1,51 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the Mage project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// Declares parallel execution utilities.
+///
+//===----------------------------------------------------------------------===//
+
+#ifndef MAGE_SUPPORT_PARALLEL_HPP
+#define MAGE_SUPPORT_PARALLEL_HPP
+
+#include "llvm/ADT/STLFunctionalExtras.h"
+
+#include <stddef.h>
+
+namespace mage {
+namespace parallel {
+
+namespace detail {
+
+void parallelize(size_t NumWorkItems, llvm::function_ref<void(size_t)> Fn);
+
+} // namespace detail
+
+/// Returns the number of threads available for parallel execution.
+[[nodiscard]] size_t getThreadCount();
+
+/// Returns the index of the thread executing the current work item.
+///
+/// This function must only be called from a function invoked by parallelize().
+[[nodiscard]] unsigned getThreadIndex();
+
+/// Invokes \p Fn for every index in [0, \p NumWorkItems), possibly in parallel,
+/// and returns after all invocations complete.
+///
+/// \p Fn may be invoked concurrently and in an unspecified order.
+template <typename FunctionTy>
+void parallelize(size_t NumWorkItems, FunctionTy &&Fn) {
+  detail::parallelize(NumWorkItems,
+                      [&Fn](size_t WorkItemIndex) { Fn(WorkItemIndex); });
+}
+
+} // namespace parallel
+} // namespace mage
+
+#endif // MAGE_SUPPORT_PARALLEL_HPP
